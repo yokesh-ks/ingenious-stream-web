d62dbd1882e7bd3b813756b1f0c60f60eb9252db
yokesh-ks <ksyokesh98@gmail.com>
2026-01-12 19:58:30 +0530

    feat(moviesuggest): layout added

--- app/movies-suggest/layout.tsx
+++ app/movies-suggest/layout.tsx
@@ -4,8 +4,8 @@ import { Footer } from "@/components/footer";
 export default function RadioLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<div className="flex-1 flex flex-col overflow-hidden h-screen">
-			<PageHeader title="TV Schedules" />
-			<main className="w-full h-full overflow-y-auto p-6 space-y-8">{children}</main>
+			<PageHeader title="Movies Suggest" />
+			<main className="w-full h-full overflow-y-auto">{children}</main>
 			<Footer />
 		</div>
 	);
--- app/movies-suggest/page.tsx
+++ app/movies-suggest/page.tsx
@@ -1,11 +1,9 @@
 export default function MoviesSuggestPage() {
 	return (
-		<div className="w-full h-full overflow-hidden bg-background">
-			<iframe
-				src="https://typebot.co/movie-suggest-vpdeq35"
-				style={{ border: "none", width: "100%", height: "100vh" }}
-				title="Movies Suggest"
-			/>
-		</div>
+		<iframe
+			src="https://typebot.co/movie-suggest-vpdeq35"
+			style={{ border: "none", width: "100%", height: "95vh" }}
+			title="Movies Suggest"
+		/>
 	);
 }
