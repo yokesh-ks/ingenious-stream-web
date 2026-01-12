9b49eecbea1a7a52e40b6b9e5ee8d2fea1df85d2
yokesh-ks <ksyokesh98@gmail.com>
2026-01-12 10:01:18 +0530

    feat: Google Analytics Setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,21 @@ export default function RootLayout({
 					}}
 					strategy="beforeInteractive"
 				/>
+				<Script
+					src="https://www.googletagmanager.com/gtag/js?id=G-5THXGYQ75F"
+					strategy="afterInteractive"
+				/>
+				<Script
+					dangerouslySetInnerHTML={{
+						__html: `
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'G-5THXGYQ75F');
+						`,
+					}}
+					strategy="afterInteractive"
+				/>
 				<ThemeProvider
 					attribute="class"
 					defaultTheme="light"
